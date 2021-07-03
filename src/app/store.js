57494a2ae070ccef0d9c63ecf4dd401f36d4b0f7
Paulina Korpak <korpak.paulina@gmail.com>
2021-07-03 17:20:58 +0100

    Create game state slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import gameReducer from '../features/game/gameSlice';
 
-export const store = configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    game: gameReducer,
+  },
 });
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+import App from './App';
+import store from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +10,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
